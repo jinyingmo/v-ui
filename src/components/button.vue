0f8de31c405beebe992de71d7a223b3ce3ba4d8f
jinyingmo <jinyingmo@eigenvr.com>
2018-09-11 18:31:35 +0800

    update button component

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,9 +1,12 @@
 <template>
-    <button :class="btn_classes"><slot></slot></button>
+    <button :class="btn_classes" :disabled="disabled" @click="handleClick">
+        <i :class="icons" v-if="loading || icon != ''"></i>
+        <span class="btn-text" v-if="!loading"><slot></slot></span>
+        <span class="btn-text" v-if="loading">loading...</span>
+    </button>
 </template>
 <script>
-    import {isType} from '../utils/utils'
-    
+    import {isType, isSize, isIcon} from '../utils/utils'
     export default {
         name:'button',
         data(){
@@ -17,16 +20,50 @@
                     return isType(value)
                 },
                 default: 'default'
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            icon: {
+                validator: function (value) {
+                    return isIcon(value)
+                },
+                default: ''
+            },
+            size: {
+                validator: function (value) {
+                    return isSize(value)
+                },
+                default: 'medium'
+            },
+            loading: {
+                type: Boolean,
+                default: false
             }
         },
         methods: {
-           
+            handleClick: function(e) {
+                if(this.loading){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    return;
+                }
+                this.$emit('click', event)
+            }
         },
         computed: {
             btn_classes() {
                 return [
                     `btn`,
-                    `btn-${this.type}`
+                    `btn-${this.type}`,
+                    `btn-${this.size}`
+                ];
+            },
+            icons() {
+                return [
+                    `btn-icon`,
+                    this.loading?'btn-icon-loading':`btn-icon-${this.icon}`
                 ];
             }
         }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,11 @@
-export function isType (value) {
+export function isType(value) {
 	return ['default', 'success', 'warning', 'error'].indexOf(value) !== -1
+}
+
+export function isSize(value) {
+	return ['large', 'medium', 'small'].indexOf(value) !== -1
+}
+
+export function isIcon(value) {
+	return ['', 'search', 'undo', 'small'].indexOf(value) !== -1
 }
